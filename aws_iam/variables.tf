30b5b5c88402ab47debab2be5b182dd3f1ecb349
Chris <infosechris@gmail.com>
2023-01-19 22:37:35 -0500

    AWS Terraform-ed - Additional comments added

--- aws_iam/variables.tf
+++ aws_iam/variables.tf
@@ -1,11 +1,9 @@
-# AWS Region
 variable "aws_region" {
     type = string
     description = "AWS Region"
     default = "us-east-1"
 }
 
-# Users
 variable "secdevops_usernames" {
   type = list(string)
   description = "List of SecDevOp's users"
--- aws_vpc/variables.tf
+++ aws_vpc/variables.tf
@@ -1,39 +1,33 @@
-# AWS Region
 variable "aws_region" {
   type    = string
   description = "AWS Region"
   default = "us-east-1"
 }
 
-# Environment
 variable "environment" {
   type    = string
   description = "Deploying environment - tagging puposes"
   default = "test"
 }
 
-# VPC CIDR Range
 variable "vpc_cidr" {
   type    = string
   description = "VPC CIDR block"
   default = "10.0.0.0/16"
 }
 
-# Public Subnet CIDR
 variable "public_subnets_cidr" {
   type    = list(any)
   description = "List of public subnet CIDR blocks"
   default = ["10.0.1.0/24"]
 }
 
-# Private Subnet CIDR
 variable "private_subnets_cidr" {
   type    = list(any)
   description = "List of private subnet CIDR blocks"
   default = ["10.0.2.0/24"]
 }
 
-# Availability Zones
 variable "availability_zones" {
   type    = list(any)
   description = "List of AZs to deploy on"
