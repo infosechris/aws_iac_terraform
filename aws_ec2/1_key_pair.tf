baae99a025b1730a712bb53320e7d6a67e861fb7
Chris <infosechris@gmail.com>
2023-01-19 22:21:32 -0500

    AWS Terraform-ed - Provider comment added

--- aws_ec2/1_key_pair.tf
+++ aws_ec2/1_key_pair.tf
@@ -1,15 +1,17 @@
-# Algorithm for the Key Pair
+# Hashicorp's tls proivider - Algorithm for the Key Pair
 resource "tls_private_key" "key_pair" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+# Hashicorp's local proivider - Save the file
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.key_pair.key_name}.pem"
+  content  = tls_private_key.key_pair.private_key_pem
+}
+
 # Create the Key Pair
 resource "aws_key_pair" "key_pair" {
   key_name   = "my-key-pair"
   public_key = tls_private_key.key_pair.public_key_openssh
-}
-# Save the file
-resource "local_file" "ssh_key" {
-  filename = "${aws_key_pair.key_pair.key_name}.pem"
-  content  = tls_private_key.key_pair.private_key_pem
 }
